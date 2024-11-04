3d7577e01f9987a0a42d5b2d6c3c4f31e4ac27f3
Krishu <varakrishna73@gmail.com>
2024-11-04 19:46:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession(); 
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddControllersWithViews();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//}
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//app.Run();
+
+////Login 
+//builder.Services.AddDistributedMemoryCache();
+//builder.Services.AddHttpContextAccessor();
+//builder.Services.AddSession();
+
+//builder.Services.AddControllersWithViews();
+
+//// Enable session middleware
+//app.UseSession(); // Must be added before app.MapControllerRoute
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+
+//// enables the authentication middleware in ASP.NET Core to handle user authentication for securing endpoints.​
+//app.UseAuthentication();
+
+//app.UseAuthorization();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//app.Run();
